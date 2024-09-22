bed90fe30eee09c7357f9001993c0d34379300c4
Munir <smsirajulmonir@gmail.com>
2024-09-22 10:20:53 -0700

    base value input displayed

---
+++ scripts/triangle-calculate.js
@@ -0,0 +1,14 @@
+/**
+ * objective: get base, height of a triangle. calculate the area by using the provided formula.
+ *  and then display the area.
+ * step-1: get base value and height value
+ * 
+ * */ 
+
+function calculateTriangleArea(){
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseInt(triangleBaseText);
+
+    console.log(base);
+}
